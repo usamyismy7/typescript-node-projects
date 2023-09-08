e1849c3dc88f9eaed532af5cd1fc08b2e44dcd4f
Usama Qazi <usamyharis@yahoo.com>
2023-09-09 01:43:00 +0500

    Added project09-countdown_timer

---
+++ project09-countdown_timer/README.md
@@ -0,0 +1,24 @@
+# Countdown Timer
+
+A countdown timer using the date module.
+
+## Installation
+
+To use this app, you need to have Node.js and npm installed on your machine. Then, run the following command in the terminal to install the app:
+
+```bash
+npx countdown_timer-usamy
+```
+
+## Technologies Used
+
+- TypeScipt
+- Node.js
+- Inquirer
+
+## Screenshots
+
+Here are some screenshots of the app in action:
+
+![Alt text](image.png)
+![Alt text](image-1.png)
---
+++ project09-countdown_timer/dist/index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const countdown = (targetDate) => {
+    const interval = setInterval(() => {
+        const currentDate = new Date();
+        const remainingTime = targetDate.getTime() - currentDate.getTime();
+        if (remainingTime <= 0) {
+            clearInterval(interval);
+            console.log("Countdown timer has ended!");
+        }
+        else {
+            const seconds = Math.floor(remainingTime / 1000);
+            console.log(`Time remaining: ${seconds} seconds`);
+        }
+    }, 1000);
+};
+const promptUser = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "targetDate",
+            message: "Enter the target date and time (YYYY-MM-DD HH:MM:SS):",
+        },
+    ]);
+    const targetDate = new Date(answers.targetDate);
+    if (isNaN(targetDate.getTime())) {
+        console.log("Invalid date format. Please use YYYY-MM-DD HH:MM:SS.");
+    }
+    else {
+        countdown(targetDate);
+    }
+};
+promptUser();
